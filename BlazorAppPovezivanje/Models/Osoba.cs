ecd240ca556fae6bab8092e746965256a4a609e5
ZeljkaMaletic <zeljka.maletic.mail@gmail.com>
2023-03-08 18:38:54 +0100

    initial commit 2

---
+++ BlazorAppPovezivanje/Models/Osoba.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorAppPovezivanje.Models
+{
+    public class Osoba
+
+    {
+     public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string   BrojTelefona { get; set; }
+        public string BrojMobitela { get; set; }
+        public string Adresa { get; set; }
+        public string Grad { get; set; }
+        public string Drzava { get; set; }
+
+    }
+}
